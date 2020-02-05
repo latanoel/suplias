7e6fbe804f96e2efc72db61c62bd37e635be3976
Isah Lata <latanoel@yahoo.com>
2020-02-05 08:46:02 +0100

    web build

---
+++ web-build/precache-manifest.8ecc0ee4f8a83f11d4edf2549edda830.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "fc6659374f86fea75636",
+    "url": "/static/js/app.5c88d6e5.chunk.js"
+  },
+  {
+    "revision": "e8db62a30e29747247cd218eeaca03f3",
+    "url": "/static/js/app.5c88d6e5.chunk.js.gz"
+  },
+  {
+    "revision": "4a8161a6a7e5c53eb7799520d09a4956",
+    "url": "/static/media/avatar.4a8161a6.jpg"
+  },
+  {
+    "revision": "8b9a8c742f50714bee4b9b54978eed83",
+    "url": "/static/media/3563071689.8b9a8c74.jpg"
+  },
+  {
+    "revision": "fecb296c26f7314ef2ef550297077139",
+    "url": "/static/js/runtime~app.80ff95d9.js.gz"
+  },
+  {
+    "revision": "d24990847fa30f465941",
+    "url": "/static/js/runtime~app.80ff95d9.js"
+  },
+  {
+    "revision": "83c42c6626d4d2bfea2dda5757365a2c",
+    "url": "/static/js/2.5c3912ff.chunk.js.gz"
+  },
+  {
+    "revision": "c949f2045f8897feb75d",
+    "url": "/static/js/2.5c3912ff.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "e9e4dcdb7e1536341febf49469f2f2dd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "3839d7879d90b127ca64f0d79509d4e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a2a8019995a0671e46de9607f5dfe6c",
+    "url": "/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "98bb6a9f670dc5a2d51c428f77539a44",
+    "url": "/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "5cd221e1f00589d415133b1afdf680e8",
+    "url": "/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "e2a1876e68b3885d03062214bc6016e9",
+    "url": "/./fonts/GraphikRegular.otf"
+  }
+];
